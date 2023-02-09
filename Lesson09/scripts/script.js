c535a3983562e36546354bb0de08afcdd160ab17
SergeyZ <szabolotnyi777@gmail.com>
2023-02-09 12:43:10 +0100

    Lesson9, javascript01

---
+++ Lesson09/scripts/script.js
@@ -0,0 +1,77 @@
+// console.log("Hello world")
+
+// let name = prompt("Write your name")
+// console.log("Hello " + name) //concatenation
+// console.log(`Hello ${name}. How are you?`)
+// let oS = prompt("What operation System (Windows, MacOS, Linux)) do you use?")
+// console.log (`Do use ${oS}, thats wonderfull`)
+
+// console.log(5)
+// console.log("5")
+
+// console.log(typeof(name))
+// console.log(typeof(5))
+// console.log(typeof name)
+// console.log(typeof 5)
+
+// const r = 12;
+// const g = 34;
+// const b = 14;
+
+// console.log(`rgb(${r}, ${g}, ${b});`)
+
+// const value_1 = 12
+// const value_2 = 4
+
+// console.log(value_1 + value_2)
+// console.log(value_1 - value_2)
+// console.log(value_1 * value_2)
+// console.log(value_1 / value_2)
+// console.log(value_1 % value_2)
+// console.log(value_1 ** value_2)
+
+// const a  = 2
+// const b = "2"
+// console.log(typeof String(a))
+// console.log(typeof Number(b))
+// console.log(typeof +b)
+// console.log(typeof ("" +a))
+
+
+// console.log("b" + "a" + +"c" +"a")
+
+// const age = prompt("Write your age ")
+// console.log(`Your age is ${age*365*24*60} minuts`) //concatenation
+
+// const number = prompt("Write some number")
+// console.log(`The ${number} to the power of 2 is ${number**2}.`)
+
+// const num_1 = +prompt("Write first number")
+// const num_2 = Number(prompt("Write second number"))
+
+// console.log(`First number + Second number = ${num_1+ num_2}`)
+
+// if (a == b){
+//     console.log("a = b")
+// }
+
+
+const a = "2"
+const b = 2
+
+console.log(a == b) // true
+console.log(a === b) // false
+
+if (a == b){
+  console.log("a = b")
+} else {
+    console.log ("a не равно b")
+}
+
+if (a > b){
+    console.log("a больше b")
+  } else if (a < b){
+      console.log ("a меньше b")
+  } else{
+    console.log ("a = равно b")
+  }
