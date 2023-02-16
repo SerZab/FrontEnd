02bc9dc26785afac5c0d30c4953f0e7c52a84388
SergeyZ <szabolotnyi777@gmail.com>
2023-02-16 12:50:15 +0100

    Lesson 13, javascript

--- Lesson13/scripts/script.js
+++ Lesson13/scripts/script.js
@@ -1,82 +1,62 @@
-function getName() {
-  const name = prompt('Write your name')
-  console.log(name);
-}
-// getName()
+// DOM = Document Object Model
 
-function checkNumber() {
-  const number = prompt("Введите число:");
-  if (number % 2 === 0) {
-    console.log("Да");
-  } else {
-    console.log("Нет");
-  }
-}
-// checkNumber()
+// const pValue = document.querySelector('p')
+// console.log(pValue.innerText)
+// pValue.innerText = 'Hello World'
+// console.log(pValue.innerText)
 
-function print(str) {
-  console.log(str)
-}
-function getMax(a, b) {
-  if (a > b) {
-    console.log(a);
-  } else {
-    console.log(b);
-  }
-}
-// getMax(3, 4)
+// const paragraghs = document.querySelectorAll('p')
+// console.log(paragraghs)
 
-
-// function power(a, b=2) {
-//   let result = a**b
-//   return result
+// for (let index = 0; index < paragraghs.length; index++) {
+//   console.log(paragraghs[index].innerText) 
 // }
-// let result = power(5, 4)
-// c
+// const pp = document.querySelector('.main .info_1')
+// pp.innerText = 'Hello World!'
 
+// const paragraphs = document.querySelectorAll(".info_1");
 
-// console.log(power(power(4) + power(3), 0.5))
-
-function check(a) {
-  return !(a % 2)
-}
-
-// console.log(check(5))
-
-// function createArr(num) {
-//   let result = [];
-//   for (let index = 0; index < num; index++) {
-//     result.push(index);
-//   }
-//   return result;
+// const newArray = [];
+// for (let index = 0; index < paragraphs.length; index++) {
+//   newArray.push(paragraphs[index].textContent);
 // }
+// console.log(newArray);
 
-// const getArray = createArr
+// const img = document.querySelector('img')
+// img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm81XoZa9dFFAFPY-LjxgJ-XAj-KeySicSvw&usqp=CAU')
+// img.setAttribute('alt', 'new photo')
+// const img1 = document.querySelector('img.photo')
+// img1.setAttribute('src', 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg')
 
-// console.log (getArray(20))
-// console.log (getArray(100))
+// const images = document.querySelectorAll('.main img');
 
-// console.log (createArr(20))
-// console.log (createArr(100))
+// for (let i = 0; i < 5; i++) {
+//   images[i].setAttribute('src', 'https://www.planetware.com/wpimages/2019/11/canada-in-pictures-beautiful-places-to-photograph-morraine-lake.jpg');
+// }
+// const divv = document.querySelector('.main')
+// const pp = document.createElement('p')
+// pp.innerText = 'Hello world!!!'
+// divv.prepend(pp) //добавляет элемент в блок до первого подобного элемента
+// divv.append(pp) //добавляет элемент в блок после последнего подобного элемента
 
-const power = function(a, b=2) {
-  return a**b
+const paragraphs = ['Day1', 'Day2', 'Day3', 'Day4'];
+
+const div = document.querySelector('#main'); //доступ к селектору
+
+for (let index = 0; index < paragraphs.length; index++) {
+  const p = document.createElement('p');
+
+  p.innerText = paragraphs[index];
+
+  div.append(p);
 }
-console.log (power(power(3) + power(4),0.5))
-cout = console.log
-const obj = {
-  print: console.log
-}
-obj.print(123)
-obj.print(120)
-obj.print(10)
-cout(2000)
 
-const createArr = function (a, b) {
-  let result = [];
-  for (let index = a; index <= b; index++) {
-    result.push(index);
-  }
-  return result;
+
+function createP(text) {
+  const p = document.createElement('p');
+  p.innerText = text;
+  return p;
 }
-console.log (createArr(3, 10))
+
+const arr = []
+
---
+++ Lesson14/scripts/script.js
@@ -0,0 +1,8 @@
+// Lesson 13
+function createP(text) {
+    const p = document.createElement('p');
+    p.innerText = text;
+    return p;
+  }
+  
+  const arr = []
